85ed421aed4f31523a23220b7cbe9bcaf57a40f5
Josh Gachnang <josh@pcsforeducation.com>
2012-09-20 22:31:02 -0500

    Adding robot.txt

--- joshgachnang/urls.py
+++ joshgachnang/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 import settings
+from django.http import HttpResponse
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,6 +9,7 @@ urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/images/favicon.ico'}),
+    (r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", mimetype="text/plain")),
 )
 # All Posts,Categories, and Tags have a space in the root domain. Check for those last.
 urlpatterns += patterns('website.views',
